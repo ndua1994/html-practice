1a26e353c8c0a22fb1995aaca16043709f136a7f
vkp6wefr <nishant.dua@neosoftmail.com>
2023-01-11 19:14:21 +0530

    session 1 and session 2

---
+++ session1.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+      <title>Session 1</title>
+</head>	
+<body>
+
+
+
+	<b><i>this is a bold tag</i></b>
+	
+
+
+	<h1>heading</h1>
+
+		<i>italic tag</i>
+
+
+		heloo <br> nishant
+
+
+		<h2>HEADING 2</H2>
+
+
+</body>
+</html>
---
+++ session2.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+       <title>Session 2</title>
+</head>
+<body>
+
+	<a href="https://www.google.com/">Google</a><br>
+
+	<a href="https://www.facebook.com/" target="_blank" title="fb">Facebook</a>
+
+
+	<p title="my name">nishant</p>
+
+
+	<img src="images/IMG_5375.jpg" width="500" height="250" alt="no image">
+	
+</body>	
+</html>
