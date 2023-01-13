b5f034a2915e15179ac0a26ae0db1571260d77a7
ndua1994 <duanishant71@gmail.com>
2023-01-13 18:16:34 +0530

    sesion 4 and 5

---
+++ session4.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	   <title>Session 4</title>
+</head>
+<body>
+
+
+<p style="color:red;text-align: center;font-size: 100px;">this is a paragraph</p>
+
+
+<h1 style="font-family: fantasy;background-color: red;">this is heading 1</h1>
+
+
+	
+</body>
+</html>
---
+++ session5.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Session 5</title>
+</head>
+<body>
+
+
+
+<!-- these all are formatting tags -->
+	<b>this is bold</b><br>
+	<strong>this is strong</strong><br>
+	<i>this is italic</i><br>
+	<em>this is Emphasized text</em><br>
+	<mark>this is mark tag</mark><br>
+	<small>this is small tag</small>
+	<p>my name is <del>nish</del> Nishant</p>
+	<p>helo my gender is <ins>male</ins></p>
+	<p>this is Superscript text 10<sup>2</sup></p>
+	<p>this is subscript text 5<sub>3</sub></p>
+
+
+   
+   <!-- my introduction -->
+	<p>My name is nishant dua i am a software engineer</p>
+
+
+</body>
+</html>
